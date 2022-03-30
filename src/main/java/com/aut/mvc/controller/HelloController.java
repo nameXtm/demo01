3e31c41713aa489b7a6a49fe50c63e2947458004
nameXtm <100192407+nameXtm@users.noreply.github.com>
2022-03-30 08:10:29 +0800

    Add files via upload

---
+++ src/main/java/com/aut/mvc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.aut.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping( "/index")
+    public String index(){
+        //返回视图名称
+        return "index";
+    }
+
+}
+
